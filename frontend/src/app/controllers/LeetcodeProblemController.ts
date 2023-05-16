af18f2a783d0a47d165fdc14817ffaaa487fde47
brandontzhang <zhangtbrandon2@gmail.com>
2023-05-15 21:57:13 -0400

    Connect deployment to backend

--- frontend/src/app/controllers/LeetcodeProblemController.ts
+++ frontend/src/app/controllers/LeetcodeProblemController.ts
@@ -1,19 +1,31 @@
 import LeetcodeProblem from "../models/LeetcodeProblem"
 
+const devMode : boolean = false;
+
+
 export default class LeetcodeProblemController {
 
-    static testUrl : string = 'http://localhost:8080/api';
+    devMode : boolean = true;
+
+    static url : string = 'http://localhost:8080/api';
+
+    constructor() {
+        if (devMode) {
+            LeetcodeProblemController.url = 'https://test-backend-386802.appspot.com/api'
+        }
+    }
+
 
     static getProblemsInCategory = async (category : string, shuffle? : boolean) : Promise<LeetcodeProblem[]> => {
         if (!shuffle) {
             shuffle = false;
         }
-        let res = await fetch(`${this.testUrl}/leetcodeproblems?category=${category}&shuffle=${shuffle}`)
+        let res = await fetch(`${this.url}/leetcodeproblems?category=${category}&shuffle=${shuffle}`)
         return res.json();
     }
 
     static getProblemById = async(id : string) : Promise<LeetcodeProblem> => {
-        let res = await fetch(`${this.testUrl}/leetcodeproblems/${id}`);
+        let res = await fetch(`${this.url}/leetcodeproblems/${id}`);
         return res.json();
     }
 
@@ -21,12 +33,12 @@ export default class LeetcodeProblemController {
         if (!shuffle) {
             shuffle = false;
         }
-        let res = await fetch(`${this.testUrl}/leetcodeproblems?shuffle=${shuffle}`);
+        let res = await fetch(`${this.url}/leetcodeproblems?shuffle=${shuffle}`);
         return res.json();
     }
 
     static addQuestion = async (problem : LeetcodeProblem) : Promise<LeetcodeProblem> =>  {
-        let res = await fetch(`${this.testUrl}/leetcodeproblems`, {
+        let res = await fetch(`${this.url}/leetcodeproblems`, {
             method: 'POST',
             headers: {
                 'content-type' : 'application/json;charset=UTF-8',
@@ -38,12 +50,12 @@ export default class LeetcodeProblemController {
     }
 
     static getCategories = async () : Promise<string[]> => {
-        let res = await fetch(`${this.testUrl}/leetcodeproblems/categories`);
+        let res = await fetch(`${this.url}/leetcodeproblems/categories`);
         return res.json();
     }
 
     static updateQuestion = async(problem : LeetcodeProblem) : Promise<LeetcodeProblem> => {
-        let res = await fetch(`${this.testUrl}/leetcodeproblems/${problem.id}`, {
+        let res = await fetch(`${this.url}/leetcodeproblems/${problem.id}`, {
             method: 'PUT',
             headers: {
                 'content-type' : 'application/json;charset=UTF-8',
@@ -55,7 +67,7 @@ export default class LeetcodeProblemController {
     }
 
     static deleteQuestion = async(problem : LeetcodeProblem) => {
-        await fetch(`${this.testUrl}/leetcodeproblems/${problem.id}`, {
+        await fetch(`${this.url}/leetcodeproblems/${problem.id}`, {
             method: 'DELETE',
             headers: {
                 'content-type' : 'application/json;charset=UTF-8',
